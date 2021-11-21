649dbe1fa8b19a6ab85521cb13aed30be7cbd225
UrosRadojicic <urke1992@yahoo.com>
2021-11-21 20:50:07 +0100

    second commit

---
+++ ClockPatience/Cards/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClockPatience.Cards
+{
+    public class Card
+    {
+        string value;
+        string suit;
+        public string Value { get => value; set => this.value = value; }
+        public string Suit { get => suit; set => suit = value; }
+
+        #region Constructor
+        public Card()
+        {
+
+        }
+        public Card(string Value,string Suit)
+        {
+            this.value = Value;
+            this.suit = Suit;
+        }
+        #endregion
+    }
+}
---
+++ ClockPatience/Cards/Deck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClockPatience.Cards
+{
+  public  class Deck
+    {
+        List<Card> deck;
+
+        public Deck()
+        {
+            deck = new List<Card>();
+
+            for (int s = 0; s < 4; s++)
+            {
+                for (int v = 0; v < 13; v++)
+                {
+
+
+                    Card card = new Card(s.ToString(), v.ToString());
+                    deck.Add(card);
+                }
+            }
+        }
+    }
+}
